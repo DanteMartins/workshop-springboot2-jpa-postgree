1a0eb59d188b5a00a1542c484ae2cbb6febb26d2
Dante Martins <Dante.Martins@yahoo.com.br>
2020-04-25 16:08:26 -0300

    Teste...12345

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -1,5 +1,5 @@
 package com.educandoweb.course;
-//Teste 1234
+//Teste 12345
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
